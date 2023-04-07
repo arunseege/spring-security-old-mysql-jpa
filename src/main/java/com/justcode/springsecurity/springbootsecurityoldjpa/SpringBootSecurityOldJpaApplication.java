6e99f110d8f246bdf15f7ff66adaebe4eff34899
arunseege <arunsphassan@gmail.com>
2023-04-07 12:34:31 +0530

    spring security using websecurityconfigureradapter with hardcoded values

---
+++ src/main/java/com/justcode/springsecurity/springbootsecurityoldjpa/SpringBootSecurityOldJpaApplication.java
@@ -0,0 +1,13 @@
+package com.justcode.springsecurity.springbootsecurityoldjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityOldJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityOldJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/justcode/springsecurity/springbootsecurityoldjpa/SpringBootSecurityOldJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.justcode.springsecurity.springbootsecurityoldjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityOldJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
